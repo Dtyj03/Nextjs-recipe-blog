aad79e8ef8f355e0348b45132b3f34b5ea6385e2
Dtyj <darrelltan2003@gmail.com>
2023-07-11 16:56:53 +0800

    Update new pagve

---
+++ components/Skeleton.js
@@ -0,0 +1,15 @@
+
+
+export default function Skeleton() {
+  return (
+    <div className="skeleton bg-cyan-600">
+        <div className="banner">
+            <div className="header">
+                <div className="content">
+
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
